d4ca089c294966462d6ab214c5148c06ad20dc73
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-08-31 20:38:40 -0400

    feat(WIP): wasm access to framebuffer

--- apps/test-app/src/lib.rs
+++ apps/test-app/src/lib.rs
@@ -1,16 +1,20 @@
-#[link(wasm_import_module = "agave")]
-extern "C" {
-    pub fn set_pixel(x: i32, y: i32, r: i32, g: i32, b: i32, a: i32);
-}
+use agave_lib::{set_pixel, RGBA};
 
 #[no_mangle]
-pub extern "C" fn _start() {
-
-}
+pub extern "C" fn _start() {}
 
 #[no_mangle]
 pub extern "C" fn update(mouse_x: i32, mouse_y: i32) {
     unsafe {
-        set_pixel(mouse_x, mouse_y, 255, 0, 0, 0);
+        set_pixel(
+            mouse_x,
+            mouse_y,
+            RGBA {
+                r: 255,
+                g: 0,
+                b: 0,
+                a: 255,
+            },
+        );
     }
 }
--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,10 +1,10 @@
 mod raw;
 
 pub struct RGBA {
-    r: i32,
-    g: i32,
-    b: i32,
-    a: i32,
+    pub r: i32,
+    pub g: i32,
+    pub b: i32,
+    pub a: i32,
 }
 pub fn set_pixel(x: i32, y: i32, color: RGBA) {
     unsafe { raw::set_pixel(x, y, color.r, color.g, color.b, color.a) }

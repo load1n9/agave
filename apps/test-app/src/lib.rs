a52fcf56c5f62900907521d4ea8d34b7a4602635
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-08-31 20:47:04 -0400

    feat(WIP): wasm access to framebuffer

--- apps/test-app/src/lib.rs
+++ apps/test-app/src/lib.rs
@@ -5,16 +5,14 @@ pub extern "C" fn _start() {}
 
 #[no_mangle]
 pub extern "C" fn update(mouse_x: i32, mouse_y: i32) {
-    unsafe {
-        set_pixel(
-            mouse_x,
-            mouse_y,
-            RGBA {
-                r: 255,
-                g: 0,
-                b: 0,
-                a: 255,
-            },
-        );
-    }
+    set_pixel(
+        mouse_x,
+        mouse_y,
+        RGBA {
+            r: if mouse_x >= 255 { 255 } else { 0 },
+            g: 0,
+            b: 255,
+            a: 255,
+        },
+    );
 }
